c4685874472979a1e00070558277f8d71c8f4b76
Takao Nosaka <takao.nosaka@contractors.roche.com>
2021-04-01 22:51:17 +0200

    ichiban

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Contact from './components/Contact/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const bgStyle = {
+        color: 'orange',
+        background:'pink',
+        height: '100%',
+    };
+
+    return (
+        <div className="App">
+            <div>
+                <nav className="topnav">
+                    <ul id="navigation">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                        <li>
+                            <Link to="/here">Here</Link>
+                        </li>
+                        <li>
+                            <Link to="/where">Where</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="main">
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                    <Route path="/contacts">
+                        <Contact />
+                    </Route>
+                </Switch>
+            </div>
+        </div>
+    );
 }
 
 export default App;
